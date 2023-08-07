def431e25c968ad10314e7253d83cff9eb79e2f6
cbj <beijun.chen@mobisport.cn>
2023-08-07 17:17:18 +0800

    protocol

--- controler/protocolHandler.go
+++ controler/protocolHandler.go
@@ -24,11 +24,68 @@ const (
 	//FileSystemRoot = "C:/Users/mobi/Desktop/tmp/Hot_sweat_season"
 )
 
-func UnzipFile(zr *zip.Reader) bean.ZipImgBean {
-	b := bean.ZipImgBean{
-		DirIndex: -1,
-		ImgSize:  0,
+var gBean = bean.ZipImgBean{
+	DirIndex: -1,
+	ImgSize:  0,
+}
+
+func copyFile(f []*zip.File, root string) {
+	fmt.Printf("zip file size=%d\n\n", len(f))
+	for _, file := range f {
+		//fmt.Printf("zip  file=%s  isDir=%d\n", file.Name, file.FileInfo().IsDir())
+		if file.FileInfo().IsDir() {
+			continue
+			//空目录
+			//subPath := filepath.Join(root, file.Name)
+			//fmt.Printf("subPath=%s\n", subPath)
+			//read, err := zip.OpenReader(subPath)
+			//if err != nil {
+			//	fmt.Errorf("")
+			//	continue
+			//}
+			//copyFile(read.File, subPath)
+			//read.Close()
+		} else {
+			name := strings.Split(file.Name, "/") //文件夹abc下文件也会遍历到：  adb/1.jpg
+			_fileName := file.Name
+			if len(name) > 1 {
+				_fileName = name[len(name)-1]
+			}
+			path := filepath.Join(root, _fileName)
+			fmt.Printf("des path=%s\n", path)
+			_fileSuffix := path2.Ext(_fileName)
+			if _, err := strconv.Atoi(_fileName[0 : len(_fileName)-len(_fileSuffix)]); err != nil {
+				continue
+			}
+			fr, err := file.Open()
+			if err != nil {
+				fr.Close()
+				continue
+			}
+			fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
+			if err != nil {
+				fw.Close()
+				fr.Close()
+				continue
+			}
+			n, err := io.Copy(fw, fr)
+			if err != nil {
+				fw.Close()
+				fr.Close()
+				continue
+			}
+			fmt.Printf("成功解压%s，写入数据%d\n\n", path, n)
+			fw.Close()
+			fr.Close()
+			gBean.ImgSize += 1
+			gBean.ImgSrc.PushBack(path)
+		}
 	}
+}
+
+func UnzipFile(zr *zip.Reader) bean.ZipImgBean {
+
+	//获取路径下项目编号最大的
 	files, _ := ioutil.ReadDir(FileSystemRoot + "/" + "img")
 	maxTmpDir := 0
 	for _, dir := range files {
@@ -42,49 +99,53 @@ func UnzipFile(zr *zip.Reader) bean.ZipImgBean {
 			fmt.Println(dir.Name())
 		}
 	}
+	//生成当前项目编号
 	maxTmpDir += 1
 	fmt.Printf("next new dir =%d\n", maxTmpDir)
 	dst := FileSystemRoot + "/" + "img" + "/" + fmt.Sprintf("%d", maxTmpDir)
 	os.MkdirAll(dst, os.ModePerm)
-	tmpFileNum := 0
-	tmpFileList := list.New()
-	for _, file := range zr.File {
-		fmt.Println("zip file =" + file.Name)
-		path := filepath.Join(dst, file.Name)
 
-		if file.FileInfo().IsDir() {
-			if err := os.MkdirAll(path, file.Mode()); err != nil {
-				return b
-			}
-			continue
-		}
-		_fileSuffix := path2.Ext(file.Name)
-		if _, err := strconv.Atoi(file.Name[0 : len(file.Name)-len(_fileSuffix)]); err != nil {
-			continue
-		}
+	gBean.DirIndex = maxTmpDir
+	gBean.ImgSize = 0
+	gBean.ImgSrc = *list.New()
+	copyFile(zr.File, dst)
 
-		fr, err := file.Open()
-		if err != nil {
-			return b
-		}
-		fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
-		if err != nil {
-			return b
-		}
-		n, err := io.Copy(fw, fr)
-		if err != nil {
-			return b
-		}
-		fmt.Printf("成功解压%s，写入数据%d\n\n", path, n)
-		fw.Close()
-		fr.Close()
-		tmpFileNum += 1
-		tmpFileList.PushBack(path)
-	}
-	b.DirIndex = maxTmpDir
-	b.ImgSize = tmpFileNum
-	b.ImgSrc = *tmpFileList
-	return b
+	//for _, file := range zr.File {
+	//
+	//	//getFile(file,dst)
+	//
+	//	fmt.Println("zip file =" + file.Name)
+	//	path := filepath.Join(dst, file.Name)
+	//
+	//	if file.FileInfo().IsDir() {
+	//		if err := os.MkdirAll(path, file.Mode()); err != nil {
+	//			return b
+	//		}
+	//		continue
+	//	}
+	//	_fileSuffix := path2.Ext(file.Name)
+	//	if _, err := strconv.Atoi(file.Name[0 : len(file.Name)-len(_fileSuffix)]); err != nil {
+	//		continue
+	//	}
+	//
+	//	fr, err := file.Open()
+	//	if err != nil {
+	//		return b
+	//	}
+	//	fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
+	//	if err != nil {
+	//		return b
+	//	}
+	//	n, err := io.Copy(fw, fr)
+	//	if err != nil {
+	//		return b
+	//	}
+	//	fmt.Printf("成功解压%s，写入数据%d\n\n", path, n)
+	//	fw.Close()
+	//	fr.Close()
+	//}
+
+	return gBean
 }
 
 func ParseAutoDeploy(b bean.AutoDeployBean) bean.DeployResBean {
@@ -119,7 +180,7 @@ func ParseAutoDeploy(b bean.AutoDeployBean) bean.DeployResBean {
 	}
 	defer in.Close()
 
-	out, err := os.OpenFile(FileSystemRoot+"/"+dstHtmlFileName, os.O_RDWR|os.O_CREATE, 0766)
+	out, err := os.OpenFile(FileSystemRoot+"/"+dstHtmlFileName, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		return res
 	}
